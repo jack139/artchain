68a0c9116ae634e155edcc4a5689525dd7a70562
gt <jack139@gmail.com>
2021-06-15 13:57:13 +0800

    优化 item modify 代码

--- cmd/http/r1/biz_item_modify.go
+++ cmd/http/r1/biz_item_modify.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 	"bytes"
 	"time"
+	"fmt"
 	"encoding/json"
 	"github.com/valyala/fasthttp"
 	"github.com/cosmos/cosmos-sdk/client"
@@ -64,30 +65,76 @@ func BizItemModify(ctx *fasthttp.RequestCtx) {
 		return		
 	}
 
-	// 是否要修改？
-	if len(itemDesc)==0 {
+	// 构建 itemImage
+	imageList := (*itemMap)["itemImage"].([]string)
+	imageData, err := json.Marshal(imageList)
+	if err != nil {
+		helper.RespError(ctx, 9005, err.Error())
+		return
+	}
+
+	// 修改链上数据
+	respData, err := itemModify(itemMap, callerAddr, 
+		itemId, itemDesc, itemDetail, itemDate, itemType, 
+		itemSubject, itemMedia, itemSize, string(imageData), "\x00", 
+		itemBasePrice, "\x00", "WAIT", "edit")
+	if err != nil {
+		helper.RespError(ctx, 9010, err.Error())
+		return
+	}
+
+
+	// 返回区块id
+	resp := map[string]interface{}{
+		"height" : (*respData)["height"].(string),  // 区块高度
+	}
+
+	helper.RespJson(ctx, &resp)
+}
+
+// string 参数填 "\x00" 表示不修改
+func itemModify(itemMap *map[string]interface{}, callerAddr string, 
+		itemId uint64, itemDesc string, itemDetail string, itemDate string, itemType string, 
+		itemSubject string, itemMedia string, itemSize string, itemImage string, AESKey string, 
+		itemBasePrice string, currentOwnerId string, status string, 
+		logText string ) (*map[string]interface{}, error) {
+
+	// 为空串用原有值填充
+	if itemDesc=="\x00" {
 		itemDesc = (*itemMap)["itemDesc"].(string)
 	}
-	if len(itemDate)==0 {
-		itemDate = (*itemMap)["itemDate"].(string)
-	}
-	if len(itemDetail)==0 {
+	if itemDetail=="\x00" {
 		itemDetail = (*itemMap)["itemDetail"].(string)
 	}
-	if len(itemType)==0 {
+	if itemDate=="\x00" {
+		itemDate = (*itemMap)["itemDate"].(string)
+	}
+	if itemType=="\x00" {
 		itemType = (*itemMap)["itemType"].(string)
 	}
-	if len(itemSubject)==0 {
+	if itemSubject=="\x00" {
 		itemSubject = (*itemMap)["itemSubject"].(string)
 	}
-	if len(itemMedia)==0 {
+	if itemMedia=="\x00" {
 		itemMedia = (*itemMap)["itemMedia"].(string)
 	}
-	if len(itemSize)==0 {
+	if itemSize=="\x00" {
 		itemSize = (*itemMap)["itemSize"].(string)
 	}
-	if len(itemBasePrice)==0 {
+	if itemImage=="\x00" {
+		itemImage = (*itemMap)["itemImage"].(string)
+	}
+	if AESKey=="\x00" {
+		AESKey = (*itemMap)["AESKey"].(string)
+	}
+	if itemBasePrice=="\x00" {
 		itemBasePrice = (*itemMap)["itemBasePrice"].(string)
+	}
+	if currentOwnerId=="\x00" {
+		currentOwnerId = (*itemMap)["currentOwnerId"].(string)
+	}
+	if status=="\x00" {
+		status = (*itemMap)["status"].(string)
 	}
 
 	/* 信号量 */
@@ -98,29 +145,18 @@ func BizItemModify(ctx *fasthttp.RequestCtx) {
 	lastDateMap := (*itemMap)["lastDate"].([]map[string]interface{})
 	lastDateMap = append(lastDateMap, map[string]interface{}{
 		"caller": callerAddr,
-		"act":  "edit",
+		"act": logText,
 		"date": time.Now().Format("2006-01-02 15:04:05"),
 	})
 	lastDate, err := json.Marshal(lastDateMap)
 	if err != nil {
-		helper.RespError(ctx, 9004, err.Error())
-		return
+		return nil, err
 	}
-
-	// 构建 itemImage
-	imageList := (*itemMap)["itemImage"].([]string)
-	imageData, err := json.Marshal(imageList)
-	if err != nil {
-		helper.RespError(ctx, 9005, err.Error())
-		return
-	}
-
 
 	// 设置 caller_addr
 	originFlagFrom, err := helper.HttpCmd.Flags().GetString(flags.FlagFrom) // 保存 --from 设置
 	if err != nil {
-		helper.RespError(ctx, 9015, err.Error())
-		return
+		return nil, err
 	}
 	helper.HttpCmd.Flags().Set(flags.FlagFrom, (*itemMap)["creator"].(string))  // 设置 --from 地址
 	defer helper.HttpCmd.Flags().Set(flags.FlagFrom, originFlagFrom)  // 结束时恢复 --from 设置
@@ -128,8 +164,7 @@ func BizItemModify(ctx *fasthttp.RequestCtx) {
 	// 获取 ctx 上下文
 	clientCtx, err := client.GetClientTxContext(helper.HttpCmd)
 	if err != nil {
-		helper.RespError(ctx, 9009, err.Error())
-		return
+		return nil, err
 	}
 
 	// 数据上链
@@ -144,16 +179,15 @@ func BizItemModify(ctx *fasthttp.RequestCtx) {
 		itemSubject, //itemSubject string, 
 		itemMedia, //itemMedia string, 
 		itemSize, //itemSize string, 
-		string(imageData), //itemImage string, 
-		(*itemMap)["AESKey"].(string), //AESKey string, 
+		itemImage, //itemImage string, 
+		AESKey, //AESKey string, 
 		itemBasePrice, //itemBasePrice string, 
-		(*itemMap)["currentOwnerId"].(string), //currentOwnerId string, 
-		"WAIT", // 修改后状态自动设置为 WAIT
+		currentOwnerId, //currentOwnerId string, 
+		status, // status 修改后状态自动设置为 WAIT
 		string(lastDate), // lastDate
 	)
 	if err := msg.ValidateBasic(); err != nil {
-		helper.RespError(ctx, 9010, err.Error())
-		return
+		return nil, err
 	}
 
 	// 设置 接收输出
@@ -162,8 +196,7 @@ func BizItemModify(ctx *fasthttp.RequestCtx) {
 
 	err = tx.GenerateOrBroadcastTxCLI(clientCtx, helper.HttpCmd.Flags(), msg)
 	if err != nil {
-		helper.RespError(ctx, 9011, err.Error())
-		return		
+		return nil, err
 	}
 
 	// 结果输出
@@ -175,20 +208,13 @@ func BizItemModify(ctx *fasthttp.RequestCtx) {
 	var respData map[string]interface{}
 
 	if err := json.Unmarshal(respBytes, &respData); err != nil {
-		helper.RespError(ctx, 9012, err.Error())
-		return
+		return nil, err
 	}
 
 	// code==0 提交成功
 	if respData["code"].(float64)!=0 { 
-		helper.RespError(ctx, 9099, buf.String())  ///  提交失败
-		return
+		return nil, fmt.Errorf("Tx fail: %s", buf.String())
 	}
 
-	// 返回区块id
-	resp := map[string]interface{}{
-		"height" : respData["height"].(string),  // 区块高度
-	}
-
-	helper.RespJson(ctx, &resp)
+	return &respData, nil
 }
